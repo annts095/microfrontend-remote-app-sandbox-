3bebaf0801d24c2b63544d5baaa6390a7d4203ed
annts095 <anantasuku@team-lab.com>
2026-01-05 11:17:04 +0900

    chore: SPAとしてビルドする

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   transpilePackages: ["@annts095/microfrontend-sandbox-ui"],
+  // SPAとしてビルド（qiankunで読み込むため）
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
